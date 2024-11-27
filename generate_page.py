f42e989e4b54ff3768e6bb4b8b704fa9cacef5bd
Vince <vince@krijnsen.com>
2024-11-27 16:29:00 +0000

    generate static content
    Leader, follower, and 'about me' section are now generated from a saved
    static.json file.
    
    Minor changes:
    
    - Improve naming
    - Add IDE settings to ignore file

--- generate_page.py
+++ generate_page.py
@@ -1,7 +1,20 @@
-import jinja2
+# generate_page.py
+#
+# Use jinja2 to load templates, then generate a single index.html file in the
+# page_source directory
 
-with open("template/index.template.html") as tf:
+import jinja2
+import json
+
+with open("portfolio_data/index.template.html") as tf:
     template = jinja2.Template(tf.read())
 
-with open("page_source/index.html", "w") as sf:
-    sf.write(template.render())
+with open("portfolio_data/static.json") as sf:
+    static_data = json.load(sf)
+
+page_data = {
+        "static": static_data,
+        }
+
+with open("portfolio_website/index.html", "w") as sf:
+    sf.write(template.render(**page_data))
